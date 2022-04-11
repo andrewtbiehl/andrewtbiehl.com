5bed6784f8189a709835b2229d0d7ae9940d150e
Andrew Biehl <70184461+andrewtbiehl@users.noreply.github.com>
2022-04-10 21:52:42 -0600

    Replace single quotes with double quotes

--- assets/js/render-email.js
+++ assets/js/render-email.js
@@ -2,17 +2,17 @@
 ---
 
 function renderEmail(localPart, ignored1, hostName, ignored2, topLevelDomain) {
-  const email = localPart + '@' + hostName + '.' + topLevelDomain;
-  const emailPrefix = 'ma' + 'il' + 'to';
-  const emailLink = document.getElementById('email-link-id');
-  emailLink.href = emailPrefix + ':' + email;
+  const email = localPart + "@" + hostName + "." + topLevelDomain;
+  const emailPrefix = "ma" + "il" + "to";
+  const emailLink = document.getElementById("email-link-id");
+  emailLink.href = emailPrefix + ":" + email;
   emailLink.textContent = email;
 }
 
 renderEmail(
-  '{{ site.email.local_part }}',
-  'this is a red herring',
-  '{{ site.email.host_name }}',
-  'this is also a red herring',
-  '{{ site.email.top_level_domain }}'
+  "{{ site.email.local_part }}",
+  "this is a red herring",
+  "{{ site.email.host_name }}",
+  "this is also a red herring",
+  "{{ site.email.top_level_domain }}"
 );
