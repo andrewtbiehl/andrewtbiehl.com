7ea413aa2b82eab435e3c7a2c87998dda75754e4
Andrew Biehl <70184461+andrewtbiehl@users.noreply.github.com>
2022-08-11 13:55:39 -0500

    Shorten scope of some temporary variables

--- _tree_sitter_ruby_binding/src/lib.rs
+++ _tree_sitter_ruby_binding/src/lib.rs
@@ -15,14 +15,16 @@ methods!(
     TreeSitterAdapterRubyBinding,
     _rtself,
     fn highlight(raw_code: RString, raw_language_str: RString) -> RString {
-        let code = raw_code.unwrap().to_string();
-        let possible_language = {
-            let language_str = raw_language_str.unwrap().to_string();
-            Language::from_str(&language_str).ok()
-        };
-        let formatted_code = match possible_language {
-            Some(language) => tree_sitter_adapter::highlight(&code, &language),
-            None => tree_sitter_adapter::no_highlight(&code),
+        let formatted_code = {
+            let code = raw_code.unwrap().to_string();
+            let possible_language = {
+                let language_str = raw_language_str.unwrap().to_string();
+                Language::from_str(&language_str).ok()
+            };
+            match possible_language {
+                Some(language) => tree_sitter_adapter::highlight(&code, &language),
+                None => tree_sitter_adapter::no_highlight(&code),
+            }
         };
         RString::new_utf8(&formatted_code)
     }
